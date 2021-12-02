8b22a649a74287e3a38bbe5283f5507f37d18a86
Yasin Sönmez <{ID}+{username}@users.noreply.github.com>
2021-12-02 18:35:46 +0100

    CLI-Table added for output

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,32 @@
 use aoc2021_lib::days::*;
-use std::fs;
 use std::path::Path;
+use cli_table::{format::Justify, print_stdout, Cell, Style, Table};
 
 fn main() {
     let day1_path = Path::new("input/day1.txt");
     let day2_path = Path::new("input/day2.txt");
 
-    let day1_str = fs::read_to_string(day1_path).unwrap();
-    let day2_str = fs::read_to_string(day2_path).unwrap();
+    let day1_str = std::fs::read_to_string(day1_path).unwrap();
+    let day2_str = std::fs::read_to_string(day2_path).unwrap();
 
     let day1_parsed = day1::parse(&day1_str);
 
-    println!("{}", day1::day1_1(&day1_parsed));
-    println!("{}\n", day1::day1_2(&day1_parsed));
+    let table = vec![
+        vec!["Day 1".cell(),
+             day1::day1_1(&day1_parsed).cell().justify(Justify::Right),
+             day1::day1_2(&day1_parsed).cell().justify(Justify::Right)
+        ],
+        vec!["Day 2".cell(),
+             day2::day2_1(&day2_str).cell().justify(Justify::Right),
+             day2::day2_2(&day2_str).cell().justify(Justify::Right)
+        ],
+    ]
+    .table()
+    .title(vec![
+        "Day".cell().bold(true),
+        "Part 1".cell().bold(true),
+        "Part 2".cell().bold(true)
+    ]).bold(true);
 
-    println!("{}", day2::day2_1(&day2_str));
-    println!("{}\n", day2::day2_2(&day2_str));
+    print_stdout(table).unwrap();
 }
